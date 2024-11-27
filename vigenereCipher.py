351298a12cf3073dff1d76049bfce87645205917
Kenchgunde Naveen <63925474+NAVEEN10032004K@users.noreply.github.com>
2024-11-28 00:17:11 +0530

    Add files via upload

---
+++ vigenereCipher.py
@@ -0,0 +1,9 @@
+message = 'n'
+key = 'a'
+
+for char in message.lower():
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  key_index = 0
+  final_message = ''
+  key_char = key_index + alphabet.find[key]
+  print(key_char)
