5cec7e9b3c4f1b16caece7abd4a94e584024c7fa
Kenchgunde Naveen <63925474+NAVEEN10032004K@users.noreply.github.com>
2024-11-30 19:26:13 +0530

    Delete vigenereCipher.py

--- vigenereCipher.py
+++
@@ -1,9 +0,0 @@
-message = 'n'
-key = 'a'
-
-for char in message.lower():
-  alphabet = 'abcdefghijklmnopqrstuvwxyz'
-  key_index = 0
-  final_message = ''
-  key_char = key_index + alphabet.find[key]
-  print(key_char)
